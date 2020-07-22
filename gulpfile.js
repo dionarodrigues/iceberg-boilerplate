f7cccfe704f1310788e99808ef31f724cccbfc5b
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-07-22 11:55:14 -0300

    Upadate gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
@@ -12,157 +12,144 @@ const prefixer = require('autoprefixer-stylus');
 const cssnano = require('gulp-cssnano');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
-const imagemin = require('gulp-imagemin');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-const plumber = require('gulp-plumber');
-const watch = require('gulp-watch');
-const batch = require('gulp-batch');
-const runSequence = require('run-sequence');
+const rename = require('gulp-rename');
 const del = require('del');
+const imagemin = require('gulp-imagemin');
+const browsersync = require('browser-sync');
+
+const { reload } = browsersync;
+const plumber = require('gulp-plumber');
+
 
 // Paths
 const paths = {
   dev: './src/',
   dest: './build/assets/',
-  build: 'build/**/*'
+  build: 'build/**/*',
 };
 
 const srcPaths = {
-  js: paths.dev + 'js/*.js',
-  jsModules: paths.dev + 'js/modules/**/*.js',
+  js: `${paths.dev}js/*.js`,
+  jsModules: `${paths.dev}js/modules/**/*.js`,
 
-  css: paths.dev + 'styl/**/*.styl',
-  styl: paths.dev + 'styl/main.styl',
+  css: `${paths.dev}styl/**/*.styl`,
+  styl: `${paths.dev}styl/main.styl`,
 
-  pug: paths.dev + 'views/**/*.pug',
-  pugPages: paths.dev + 'views/pages/**/*.pug',
+  pug: `${paths.dev}views/**/*.pug`,
+  pugPages: `${paths.dev}views/pages/**/*.pug`,
 
-  img: paths.dev + 'img/**/*.{jpg,png,gif,svg}',
-  fonts: paths.dev + 'fonts/*',
+  img: `${paths.dev}img/**/*.{jpg,png,gif,svg}`,
+  fonts: `${paths.dev}fonts/*`,
 };
 
 const buildPaths = {
-  build: paths.dest + '**/*',
-  js: paths.dest + 'js/',
-  css: paths.dest + 'css/',
+  build: `${paths.dest}**/*`,
+  js: `${paths.dest}js/`,
+  css: `${paths.dest}css/`,
   pug: './build/',
-  img: paths.dest + 'img',
-  fonts: paths.dest + 'css/fonts',
+  img: `${paths.dest}img`,
+  fonts: `${paths.dest}css/fonts`,
 };
 
 // Clean all 'dest' directory before generating the files
-gulp.task('clean', () => {
-  return del( paths.build );
-});[
-  '/path/to/app.js',
-  '/path/to/mymodule/mymodule.js',
-  '/path/to/mymodule/mymodule/*.js'
-]
+function clean() {
+  return del([buildPaths.build]);
+}
 
 // Pug Task
-gulp.task('html', () => {
-  return gulp.src(srcPaths.pugPages)
+function html() {
+  return gulp
+    .src(srcPaths.pugPages)
     .pipe(plumber())
     .pipe(pug({
       client: false,
-      pretty: true
+      pretty: true,
     }))
     .pipe(gulp.dest(buildPaths.pug))
     .pipe(reload({ stream: true }));
-});
+}
 
 // CSS task
-gulp.task('css', () => {
-  return gulp.src(srcPaths.styl)
+function css() {
+  return gulp
+    .src(srcPaths.styl)
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(stylus({
-      use: [prefixer(), jeet(), rupture()]
+      use: [prefixer(), jeet(), rupture()],
     }))
     .pipe(rucksack())
-    .pipe(cssnano()) //--> minify css
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest(buildPaths.css))
+    .pipe(cssnano()) // --> minify css
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(browsersync.reload({ stream: true }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(buildPaths.css))
     .pipe(reload({ stream: true }));
-});
+}
 
 // Fonts task
-gulp.task('fonts', () => {
+function fonts() {
   return gulp.src(srcPaths.fonts)
     .pipe(plumber())
     .pipe(gulp.dest(buildPaths.fonts))
     .pipe(reload({ stream: true }));
-});
+}
 
 // Javascript Task
-gulp.task('js', () => {
+function js() {
   return gulp.src([srcPaths.jsModules, srcPaths.js])
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(babel({
-      presets: ['env']
+      presets: ['env'],
     }))
     .pipe(concat('main.js'))
+    .pipe(gulp.dest(buildPaths.js))
     .pipe(uglify())
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(browsersync.reload({ stream: true }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(buildPaths.js))
     .pipe(reload({ stream: true }));
-});
+}
 
 // Img Task
-gulp.task('img', () => {
+function images() {
   return gulp.src(srcPaths.img)
     .pipe(plumber())
-    .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true, cache: false }))
+    .pipe(imagemin({
+      optimizationLevel: 5, progressive: true, interlaced: true, cache: false,
+    }))
     .pipe(gulp.dest(buildPaths.img))
     .pipe(reload({ stream: true }));
-});
+}
 
 // Watch stylus files, js files, img files and pug files for changes and recompile
-gulp.task('watch', () => {
-  watch(srcPaths.css, batch(function (event, done) {
-    gulp.start('css', done);
-  }));
+function watchFiles() {
+  gulp.watch(srcPaths.css, css);
+  gulp.watch([srcPaths.jsModules, srcPaths.js], js);
+  gulp.watch(srcPaths.img, images);
+  gulp.watch(srcPaths.pug, html);
+}
 
-  watch([srcPaths.jsModules, srcPaths.js], batch(function (event, done) {
-    gulp.start('js', done);
-  }));
-
-  watch(srcPaths.img, batch(function (event, done) {
-    gulp.start('img', done);
-  }));
-
-  watch(srcPaths.pug, batch(function (event, done) {
-    gulp.start('html', done);
-  }));
-});
-
-// Wait for html, then launch the Server
-gulp.task('browser-sync', ['html'], () => {
-  browserSync.init({
+// Launch the Server
+function browserSync(done) {
+  browsersync.init({
     server: {
-      baseDir: './build'
-    }
+      baseDir: './build',
+    },
+    open: true,
   });
-});
+  done();
+}
+
+// Define complex tasks
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts, js));
+const watch = gulp.parallel(build, watchFiles, browserSync);
 
 // Default and Build tasks
-gulp.task('build', ['clean'], () => {
-  gulp.start(
-    'html',
-    'js',
-    'css',
-    'img',
-    'fonts'
-  );
-});
-
-gulp.task('default', ['build'], () => {
-  gulp.start(
-    'watch',
-    'browser-sync'
-  );
-});
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
